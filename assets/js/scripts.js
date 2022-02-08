5c344b401d761dd4a80b1337e3636650d9cd4ae7
Ethan Brockway <ebrockway01@gmail.com>
2022-02-07 19:38:02 -0600

    figuring stuff out

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,13 +2,7 @@ var convert = function () {
   var converterApi =
     "https://akshayanand.herokuapp.com/api/unit/?type=type&from=from&to=to&value=value";
 
-    fetch(converterApi).then(function (responce) {
-if (responce.ok) {
-    responce.json().then(data) {
-        console.log(data);
-    }
-}
-    })
- 
+  fetch(converterApi).then(function (response) {
+    console.log(response);
+  });
 };
-convert();
