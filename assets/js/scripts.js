9e4941829c17e411d9e8a235f39d0f4312bbec45
Skronkie <ebrockway01@gmail.com>
2022-02-08 13:08:24 -0600

    commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 var convert = function () {
   var converterApi =
-    "https://akshayanand.herokuapp.com/api/unit/?type=type&from=from&to=to&value=value";
+    "https://akshayanand.herokuapp.com/api/unit/?from=kgms&to=gms&value=1";
 
   fetch(converterApi).then(function (response) {
     console.log(response);
