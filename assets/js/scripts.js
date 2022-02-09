d37abcdcfce437f1632e09bc8a97b47a9ea7d88d
Skronkie <ebrockway01@gmail.com>
2022-02-09 14:54:05 -0600

    added enter key functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 var apiKey = "uBGwsY4j0LWUoSuGrIAM6CknwEkLZxNT";
 var imgGiphy = document.getElementById("giphy-img");
+var searchedTerm = document.getElementById("search-form");
 
 var giphySearch = function (keyword) {
   var url = `https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apiKey}&limit=1`;
@@ -15,11 +16,20 @@ var giphySearch = function (keyword) {
   });
 };
 
+// Runs when button is pressed OR when user hits enter
 function onFormSubmit() {
-  let searchedTerm = document.getElementById("search-form").value;
-
-  giphySearch(searchedTerm);
+  var searchVal = searchedTerm.value;
+  // Runs giphySearch function
+  giphySearch(searchVal);
 }
-giphySearch();
+searchedTerm.addEventListener("keydown", function (event) {
+  // checks if the enter key was the key pressed.
+  if (event.key === "Enter") {
+    event.preventDefault();
 
+    // Trigger the button element with a click+
+
+    document.getElementById("btn").click();
+  }
+});
 btn.addEventListener("click", onFormSubmit);
