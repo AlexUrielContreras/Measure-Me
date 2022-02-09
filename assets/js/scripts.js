6209729fc37cfd94bcbb5288881d8feb2b155568
Skronkie <ebrockway01@gmail.com>
2022-02-08 18:14:43 -0600

    fetched APi

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,21 @@
-var convert = function () {
-  var converterApi =
-    "https://akshayanand.herokuapp.com/api/unit/?from=kgms&to=gms&value=1";
+var apiKey = "uBGwsY4j0LWUoSuGrIAM6CknwEkLZxNT";
+var searchForm = document.getElementById("#search-form");
 
-  fetch(converterApi).then(function (response) {
-    console.log(response);
+var giphySearch = function (keyword) {
+  var url = `https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apiKey}&limit=1`;
+  fetch(url).then(function (response) {
+    if (response.ok) {
+      response.json().then((data) => {
+        const rUrl = data.data[0].url;
+        console.log(rUrl);
+      });
+    }
   });
 };
+
+(function listenOnFormSubmit() {
+  searchForm.submit((ev) => {
+    ev.preventDefault();
+    let;
+  });
+});
