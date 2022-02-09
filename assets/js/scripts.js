fb60f65fc22517eaa46d5ea54ac32abc33c2bb8c
Skronkie <ebrockway01@gmail.com>
2022-02-08 20:01:37 -0600

    fetched url and displayed image

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,25 @@
 var apiKey = "uBGwsY4j0LWUoSuGrIAM6CknwEkLZxNT";
-var searchForm = document.getElementById("#search-form");
+var imgGiphy = document.getElementById("giphy-img");
 
 var giphySearch = function (keyword) {
   var url = `https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apiKey}&limit=1`;
+
   fetch(url).then(function (response) {
     if (response.ok) {
       response.json().then((data) => {
-        const rUrl = data.data[0].url;
-        console.log(rUrl);
+        const rUrl = data.data[0].images.original.url;
+        console.log(data.data[0]);
+        imgGiphy.src = rUrl;
       });
     }
   });
 };
 
-(function listenOnFormSubmit() {
-  searchForm.submit((ev) => {
-    ev.preventDefault();
-    let;
-  });
-});
+function onFormSubmit() {
+  let searchedTerm = document.getElementById("search-form").value;
+
+  giphySearch(searchedTerm);
+}
+giphySearch();
+
+btn.addEventListener("click", onFormSubmit);
